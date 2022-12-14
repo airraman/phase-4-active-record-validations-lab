8264a8ae5581f489e5a12ba2a18a8455fbbf8637
Raman <ramanmcreates@gmail.com>
2022-12-14 15:44:41 -0500

    commit tree 6f015b36957498cebfb67fc0827ae1ddf4cd5011

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :title_case
+
+    def title_case
+        if title === "True Facts"
+            errors.add(:title, "Can't include true facts")
+        end
+    end 
+
+
 end
